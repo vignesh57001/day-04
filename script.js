d4ba15d8b77711bab7ebf3a9b29374917350076c
vignesh57001 <109303622+vignesh57001@users.noreply.github.com>
2022-07-21 23:38:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ request.onload=function(){
 
 for(var i=0;i<result.length;i++){
       console.log(result[i].name);
-      console.log(result[i].flag);
-      
+      console.log(result[i].region);
+      console.log(result[i].subregion);
+      console.log(result[i].population);
 }
 }
  
