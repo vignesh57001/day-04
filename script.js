1ec1c9ff10a895341898283ad076331333a48a0c
vignesh57001 <109303622+vignesh57001@users.noreply.github.com>
2022-07-21 11:25:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+
+
+
+-----------------------------------------------------------------------------------------------
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="scriptres.js"></script>
+</body>
+</html>
+
+
+-----------------------------------------------------------------------------------
+
+
 var request=new XMLHttpRequest();
 request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
 request.send();
@@ -16,3 +38,20 @@ for(var i=0;i<result.length;i++){
 }
 }
  
+---------------------------------------------------------------------------------------------
+    
+    var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+
+    console.log(result[0].name);
+
+
+for(var i=0;i<result.length;i++){
+      console.log(result[i].name);
+      console.log(result[i].flag);
+}
+}
