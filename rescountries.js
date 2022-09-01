a065e1cb15840edc1adb06ba537d44e6fccf3200
vignesh57001 <109303622+vignesh57001@users.noreply.github.com>
2022-09-01 11:22:40 -0700

    Add files via upload

---
+++ rescountries.js
@@ -0,0 +1,23 @@
+
+
+
+//name,region,subregion,population..
+
+
+var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+
+    console.log(result[0].name);
+
+
+for(var i=0;i<result.length;i++){
+      console.log(result[i].name);
+      console.log(result[i].region);
+      console.log(result[i].subregion);
+      console.log(result[i].population);
+}
+}
